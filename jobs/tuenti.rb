2ed636a3606883686dcf4e1399c968bd8ba41573
Marcio Barrios <marciobarrios@gmail.com>
2013-03-07 21:40:52 +0100

    testing...

--- jobs/tuenti.rb
+++ jobs/tuenti.rb
@@ -8,9 +8,11 @@ SCHEDULER.every '10m', :first_in => 0 do |job|
 	response = http.request(request)
 	data = JSON.parse(response.body)
 
-	last_n_chats_pr0_1day = current_n_chats_pr0_1day
+	#last_n_chats_pr0_1day = current_n_chats_pr0_1day
+	last_n_chats_pr0_1day = 100
 	last_n_chats_cup_1day = current_n_chats_cup_1day
-	current_n_chats_pr0_1day = data.chat_messages_pr0_1day
+	#current_n_chats_pr0_1day = data.chat_messages_pr0_1day
+	current_n_chats_pr0_1day = 99
 	current_n_chats_cup_1day = data.chat_messages_cup_1day
 
   send_event('n_chats_pr0_1day', { current: current_n_chats_pr0_1day, last: last_n_chats_pr0_1day })
