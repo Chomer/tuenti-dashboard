aa9f879686616f74e5103b79c3a7b4715dcac803
Marcio Barrios <marciobarrios@gmail.com>
2013-03-08 02:55:33 +0100

    Modified widgets size to fit better in panoramic screens

--- jobs/instagram.rb
+++ jobs/instagram.rb
@@ -13,22 +13,19 @@ SCHEDULER.every '10s', :first_in => 0 do |job|
 	http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
 	request = Net::HTTP::Get.new(uri.request_uri)
-
 	response = http.request(request)
-
 	photos = JSON.parse(response.body)["data"]
 	n = rand(photos.length)
 
 	# if photos[0]
- #    photos[0].map! do |photo| 
+ #    photos[0].map! do |photo|
  #      { iname: photo['user']['username'], iphoto: photo['images']['low_resolution']['url'] }
  #    end
-  
+
  #    send_event('instagram', photos )
  #    #send_event('instagram', iname: response.body )
  #  end
- 
+
 	send_event('instagram', { iname: photos[n]['user']['username'], itext: photos[n]['caption']['text'], iphoto: photos[n]['images']['low_resolution']['url'] } )
 
-  #send_event('instagram', { iname: "Marcio", itext: "Lorem Ipsum Dolor sit amet", iphoto: "http://distilleryimage0.s3.amazonaws.com/fdb2fca8333311e2aa0322000a1fa408_7.jpg" })
 end
