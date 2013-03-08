d18a95e67a36d705e2195eccaaed69930c5e228c
Marcio Barrios <marciobarrios@gmail.com>
2013-03-08 11:52:45 +0100

    Changed interval to check new data

--- jobs/tuenti.rb
+++ jobs/tuenti.rb
@@ -16,7 +16,7 @@ current_status_posts_cup_1day     = 0
 current_photo_views_pr0_1day      = 0
 current_photo_views_cup_1day      = 0
 
-SCHEDULER.every '1d', :first_in => 0 do |job|
+SCHEDULER.every '60m', :first_in => 0 do |job|
 
 	uri = URI.parse("http://10.222.0.2/desktop/growth/cupcake_kpi_global_dashboard.json")
 	response = Net::HTTP.get_response(uri)
